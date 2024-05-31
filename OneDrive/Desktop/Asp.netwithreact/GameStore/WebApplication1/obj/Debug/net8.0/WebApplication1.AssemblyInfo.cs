16b3d9ac09cd693c380ec70b386e9393147027fb
Vidhi Shah <vidhishah2207@gmail.com>
2024-05-31 12:52:23 +1000

    =>add enitty framework core design =>Add Migrations

--- OneDrive/Desktop/Asp.netwithreact/GameStore/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ OneDrive/Desktop/Asp.netwithreact/GameStore/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94685482dff048b7e4501e41a34e18be47b81a8d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+037484a8fd7279f693ba9686d73999b9227a159b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
